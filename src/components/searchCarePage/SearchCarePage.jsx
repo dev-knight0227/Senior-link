76a6f64f9d4557dc579aa31126450c22da59b88c
Leo <locklee0418@proton.me>
2025-05-22 00:54:50 +0900

    modified some parts

--- src/components/searchCarePage/SearchCarePage.jsx
+++ src/components/searchCarePage/SearchCarePage.jsx
@@ -253,13 +253,13 @@ const SearchCare = ({category = "all"}) => {
                         d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
                       />
                     </svg>
-                    Filters
+                    {messages['filterTitle']}
                   </h2>
                   <button
                     onClick={resetFilters}
                     className="text-gray-500 hover:text-gray-700 text-sm font-medium"
                   >
-                    Reset
+                    {messages['resetTitle']}
                   </button>
                 </div>
 
